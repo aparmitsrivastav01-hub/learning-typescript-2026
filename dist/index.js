78aee399ee7810c0234357a8556cb440828982a0
Aparmit <aparmitsrivastav01@gmail.com>
2026-07-01 18:39:09 +0530

    Initial commit

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(person) {
+    return `hello ${person}, Welcome to learn TS`;
+}
+const username = "Chai aur TYPESCRIPT";
+console.log(greet(username));
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+function greet(person:string):string{
+    return `hello ${person}, Welcome to learn TS`
+}
+
+const username: string = "Chai aur TYPESCRIPT"
+console.log(greet(username))
