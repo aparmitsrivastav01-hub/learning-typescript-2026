c7a42a4ec3cc02f64e7aa9bc051b74e05ddfbb58
blackcatykfitness <aparmitsrivastav01@gmail.com>
2026-07-20 17:01:48 +0530

    generics

--- src/intergaceAndGen.ts
+++ src/intergaceAndGen.ts
@@ -1,11 +1,50 @@
 interface Chai {
-    flavor:string
-    price:number
+    flavor:string;
+    price:number;
+    milk?: boolean;
 }
 
 const masala:Chai = {
     flavor: "masala",
     price: 30
+};
+
+interface Shop{
+    readonly id: number
+    name: string
 }
 
-const boring = "tmlkc"
+const S:Shop = {id: 1, name: "chailele"}
+
+// s.id =2 
+
+interface DiscountCalculator{
+    (price:number): number
+}
+
+const apply50: DiscountCalculator = (p) => p*0.05
+
+interface TeaMachine{
+    start(): void;
+    stop(): void;
+}
+
+const machine: TeaMachine = {
+    start(){
+        console.log("start");
+        
+    }
+    stop(){
+        console.log("stop");
+        
+    }
+}
+
+
+//index signature 
+
+interface ChaiRating{
+    [flavor: string]: number
+}
+
+
